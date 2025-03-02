21295cca23320faf3f9e1b6871e29937c2e5d1fe
MThuyet <mthuyet294@gmail.com>
2025-03-03 00:38:55 +0700

    setup font-family and fontawesome for project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 function App() {
-  return (
-    <h1 className="text-center text-3xl font-bold text-blue-500">Movie App</h1>
-  );
+  return <h1 className="text-center font-medium">Movie App</h1>;
 }
 
 export default App;
