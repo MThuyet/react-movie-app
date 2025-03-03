d81503be773c8a05a6c58ed1627b395547af543b
MThuyet <mthuyet294@gmail.com>
2025-03-03 11:34:24 +0700

    build UI header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
+import { IoIosSearch } from "react-icons/io";
+
 function App() {
-  return <h1 className="text-center font-medium">Movie App</h1>;
+  return (
+    <div>
+      <header className="flex items-center justify-between bg-slate-950 px-9 py-4 text-white">
+        <div className="flex items-center gap-5">
+          <img
+            src="../public/logoNetflix.png"
+            alt="Netflix"
+            className="w-16 sm:w-28"
+          />
+          <a href="#" className="sm:px-2 sm:py-2.5">
+            Phim
+          </a>
+          <a href="#" className="sm:px-2 sm:py-2.5">
+            Truyền hình
+          </a>
+        </div>
+        <IoIosSearch className="size-6 cursor-pointer" />
+      </header>
+    </div>
+  );
 }
 
 export default App;
